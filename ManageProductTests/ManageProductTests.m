6d65f5f26c4981c343bd05bd27c80d55576d1334
Sonvn <vungocson94@gmail.com>
2014-04-21 14:43:56 +0700

    Initial Commit

---
+++ ManageProductTests/ManageProductTests.m
@@ -0,0 +1,34 @@
+//
+//  ManageProductTests.m
+//  ManageProductTests
+//
+//  Created by Sonvn on 4/21/14.
+//  Copyright (c) 2014 MAC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ManageProductTests : XCTestCase
+
+@end
+
+@implementation ManageProductTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
