b57dc4985bb2312965ce42cbd76b0215388a9dfd
Ngọc Nguyên <k15bhahaha@gmail.com>
2014-04-22 18:00:22 +0700

    Add some revelant classes and delete all Xcode user data

---
+++ ManageProduct/CategoryTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  CategoryTableViewController.h
+//  ManageProduct
+//
+//  Created by Ngọc Nguyên on 4/22/14.
+//  Copyright (c) 2014 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoryTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+@end
---
+++ ManageProduct/ProductTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProductTableViewController.h
+//  ManageProduct
+//
+//  Created by Ngọc Nguyên on 4/22/14.
+//  Copyright (c) 2014 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProductTableViewController : UITableViewController
+
+@end
